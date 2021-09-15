93e2d1e271fcc1e022c91a3fa83893affcc47ad6
matumoto-yu <songbenyuzai@gmail.com>
2021-09-15 17:50:42 +0900

    add comment and delete Log method in BindingAdapters.kt

--- app/src/main/java/com/example/kotlinmessenger/view/Adapter/BindingAdapters.kt
+++ app/src/main/java/com/example/kotlinmessenger/view/Adapter/BindingAdapters.kt
@@ -10,15 +10,16 @@ import de.hdodenhof.circleimageview.CircleImageView
 
 object BindingAdapters {
 
+    // bitmap設定後Viewへ画像表示
     @BindingAdapter("bitmap")
     @JvmStatic
     fun imageBitmapSet(view: CircleImageView, bitmap: Bitmap?) {
-//        Log.d("log", "imageBitmapSet start")
         if (bitmap != null) {
             view.setImageBitmap(bitmap)
         }
     }
 
+    // ChatLogでメッセージを追加したら最新メッセージへ自動スクロール
     @BindingAdapter("chatPageScroll")
     @JvmStatic
     fun scroll(view: RecyclerView, position: Int) {
